52bb61442180e2a66dd835b98aa3a87b1b4c8f31
LFN <liangfengning@foxmail.com>
2021-06-04 23:40:35 +0800

    重构代码，精简组件

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,58 +1,46 @@
-import { Link } from "gatsby"
-import React, { useEffect, useState } from "react"
+import { graphql, StaticQuery } from "gatsby"
+import React from "react"
 import Header from "../components/header"
-import Main from "../components/main"
 import Seo from "../components/seo"
-import "./404.less"
 
-const NotFoundPage = () => {
-  const [url, setUrl] = useState({ href: "/404", pathname: "/404" })
-  useEffect(() => {
-    setUrl({
-      href: window.location.href,
-      pathname: window.location.pathname,
-    })
-  }, [])
+const Message = () => (
+  <StaticQuery
+    query={MessageQuery}
+    render={data => (
+      <article
+        dangerouslySetInnerHTML={{
+          __html: data.allMarkdownRemark.edges[0].node.html,
+        }}
+      ></article>
+    )}
+  />
+)
 
-  return (
-    <>
-      <Seo title="404 Not found" />
+const NotFoundPage = () => (
+  <>
+    <Seo title="404 Not found" />
+    <div className="container">
       <Header back />
-      <Main
-        main={
-          <>
-            <article id="main-content">
-              <p className="article-subtitle error">404 / PAGE NOT FOUND</p>
-              <h1 className="not-found-title">你要找的页面不在这里 :(</h1>
-              <strong>Tips</strong>
-              <ul className="not-found-list">
-                <li>
-                  你正在访问&nbsp;
-                  <a href={url.href}>{url.pathname}</a>
-                  ，请检查一下链接有没有写错
-                </li>
-                <li>
-                  有可能是之前的页面已经被弃用，或者被修改了链接地址，去&nbsp;
-                  <Link to="/">主页</Link>&nbsp;找找看吧
-                </li>
-                <li>
-                  也可以通过&nbsp;
-                  <a
-                    href="mailto:liangfengning@foxmail.com"
-                    title="liangfengning@foxmail.com"
-                    target="_blank"
-                    rel="nofollow noopener noreferrer"
-                  >
-                    邮箱
-                  </a>
-                  &nbsp;联系我
-                </li>
-              </ul>
-            </article>
-          </>
-        }
-      />
-    </>
-  )
-}
+      <main>
+        <Message />
+      </main>
+    </div>
+  </>
+)
+
 export default NotFoundPage
+
+const MessageQuery = graphql`
+  {
+    allMarkdownRemark(filter: { frontmatter: { path: { eq: "404" } } }) {
+      edges {
+        node {
+          frontmatter {
+            path
+          }
+          html
+        }
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,154 +1,51 @@
 import { graphql, StaticQuery } from "gatsby"
 import React from "react"
-import { StringParam, useQueryParam } from "use-query-params"
-import BlogPost from "../components/blog-post"
+import Cards from "../components/cards"
 import Footer from "../components/footer"
 import Header from "../components/header"
-import Main from "../components/main"
-import { HomeNote } from "../components/notes"
-import { UpdateNow } from "../components/notification"
 import Seo from "../components/seo"
 import "./global.less"
 import "./highlight.less"
-import "./index.less"
+import "./main.less"
 import "./variables.less"
 
-const PostList = ({ tag }) => {
-  return (
-    <StaticQuery
-      query={IndexQuery}
-      render={data => {
-        let hrFlag = false
-        let articleCount = 0
-        const tags = new Map()
+const Message = () => (
+  <StaticQuery
+    query={MessageQuery}
+    render={data => (
+      <article
+        dangerouslySetInnerHTML={{
+          __html: data.allMarkdownRemark.edges[0].node.html,
+        }}
+      ></article>
+    )}
+  />
+)
 
-        if (tag) {
-          data.allMarkdownRemark.edges.forEach(({ node }) =>
-            node.frontmatter.tags.forEach(nodeTag => {
-              const slug = nodeTag.toLowerCase().replace(" ", "-")
-              tags.set(slug, nodeTag)
-              if (slug === tag) {
-                articleCount++
-              }
-            })
-          )
-        }
-
-        return (
-          <>
-            {tag && (
-              <>
-                <Seo title={`标签：${tags.get(tag) || "Unknown"}`} />
-                <section>
-                  <p className="article-subtitle">标签 / {tag || "Unknown"}</p>
-
-                  <h1>{`${articleCount} 篇文章`}</h1>
-                </section>
-              </>
-            )}
-            <div className="post-list" id="main-content">
-              {data.allMarkdownRemark.edges.map(({ node }) => {
-                const postTags = node.frontmatter.tags.map(tag =>
-                  tag.toLowerCase().replace(" ", "-")
-                )
-
-                if (!tag || postTags.includes(tag)) {
-                  let Hr = null
-                  if (hrFlag) {
-                    Hr = <hr />
-                  }
-
-                  hrFlag = true
-                  return (
-                    <React.Fragment key={node.frontmatter.title}>
-                      {Hr}
-                      <BlogPost
-                        title={node.frontmatter.title}
-                        name={node.frontmatter.name}
-                        description={node.frontmatter.description}
-                        descriptionHTML={node.fields.description_html}
-                        date={node.frontmatter.date}
-                        create_date={node.frontmatter.create_date}
-                        tags={node.frontmatter.tags}
-                        license={node.frontmatter.license}
-                        excerpt={node.excerpt}
-                      />
-                    </React.Fragment>
-                  )
-                }
-
-                return (
-                  <BlogPost
-                    hide="true"
-                    title={node.frontmatter.title}
-                    name={node.frontmatter.name}
-                    description={node.frontmatter.description}
-                    descriptionHTML={node.fields.description_html}
-                    date={node.frontmatter.date}
-                    create_date={node.frontmatter.create_date}
-                    tags={node.frontmatter.tags}
-                    license={node.frontmatter.license}
-                    excerpt={node.excerpt}
-                    key={node.frontmatter.title}
-                  />
-                )
-              })}
-            </div>
-          </>
-        )
-      }}
-    />
-  )
-}
-
-const IndexPage = () => {
-  const tag = useQueryParam("tag", StringParam)[0]
-
-  return (
-    <>
-      <Seo title="主页" />
-      <Header app aside={{ type: "note" }} />
-      <Main
-        aside={!tag && <HomeNote />}
-        main={
-          <>
-            <UpdateNow />
-            <PostList tag={tag} />
-            {!tag && <Footer />}
-          </>
-        }
-      />
-    </>
-  )
-}
+const IndexPage = () => (
+  <>
+    <Seo title="主页" />
+    <div className="container">
+      <Header app />
+      <aside>
+        <Message />
+        <Footer />
+      </aside>
+      <main>
+        <Cards />
+      </main>
+    </div>
+  </>
+)
 
 export default IndexPage
 
-const IndexQuery = graphql`
-  query {
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/notebook/" } }
-      sort: {
-        fields: [frontmatter___date, frontmatter___create_date]
-        order: [DESC, DESC]
-      }
-    ) {
+const MessageQuery = graphql`
+  {
+    allMarkdownRemark(filter: { frontmatter: { path: { eq: "home" } } }) {
       edges {
         node {
-          id
-          fields {
-            description_html
-          }
-          frontmatter {
-            date(formatString: "YYYY 年 M 月 D 日")
-            create_date(formatString: "YYYY 年 M 月 D 日")
-            title
-            name
-            tags
-            description
-            license
-          }
-          excerpt(format: HTML, pruneLength: 100)
+          html
         }
       }
     }
