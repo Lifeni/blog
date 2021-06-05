f2d05802213e47c73def7340efc2c5b7be468eb7
LFN <liangfengning@foxmail.com>
2021-06-05 20:29:26 +0800

    重构文章页

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -23,7 +23,7 @@ const domOperation = location => {
   if (block.length) {
     block.forEach(e => {
       const copy = document.createElement("button")
-      copy.className = "code-copy-button"
+      copy.className = "copy"
       copy.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>`
       copy.setAttribute("aria-label", "Copy")
       copy.title = "Copy"
@@ -43,8 +43,6 @@ const domOperation = location => {
       e.insertBefore(copy, inner)
     })
   }
-
-
 }
 
 const onRouteUpdate = ({ location, prevLocation }) => {
