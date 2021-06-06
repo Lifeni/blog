036687f7abfde34327d16cc87d743633db6450aa
LFN <liangfengning@foxmail.com>
2021-06-06 17:05:20 +0800

    迁移 gatsby-browser 到 React Hooks

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,4 +1,5 @@
 import { graphql } from "gatsby"
+import mediumZoom from "medium-zoom"
 import React, { useEffect, useRef } from "react"
 import Comment from "../components/comment"
 import Seo from "../components/seo"
@@ -33,6 +34,7 @@ const ArticleMeta = ({ frontmatter }) => {
 const ArticlePage = ({ data }) => {
   const post = data.markdownRemark
   const articleRef = useRef()
+
   useEffect(() => {
     const tables = articleRef.current.querySelectorAll("table")
     tables.forEach(table => {
@@ -42,6 +44,50 @@ const ArticlePage = ({ data }) => {
       wrapper.appendChild(clone)
       table.replaceWith(wrapper)
     })
+  }, [data])
+
+  useEffect(() => {
+    const imgs = articleRef.current.querySelectorAll("img")
+    imgs.forEach(e => {
+      e.setAttribute("tabindex", "0")
+      e.onkeypress = event => {
+        if (event.key === "Enter") {
+          e.click()
+        }
+      }
+    })
+    setTimeout(() => {
+      mediumZoom(imgs, {
+        background: "rgba(0, 0, 0, .8)",
+      })
+    }, 300)
+  }, [data])
+
+  useEffect(() => {
+    const block = articleRef.current.querySelectorAll(".gatsby-highlight")
+    if (block.length) {
+      block.forEach(e => {
+        const copy = document.createElement("button")
+        copy.className = "copy"
+        copy.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>`
+        copy.setAttribute("aria-label", "Copy")
+        copy.title = "Copy"
+        copy.onclick = () => {
+          const code = e.querySelector("pre code")
+          navigator.clipboard
+            .writeText(code.textContent)
+            .then(() => {
+              copy.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"/></svg>`
+            })
+            .catch(err => {
+              console.log("复制出错", err)
+            })
+        }
+
+        const inner = e.firstElementChild
+        e.insertBefore(copy, inner)
+      })
+    }
   }, [data])
 
   return (
