4b6e51fe8a6d4957c53ada0e1f3b69d75245b6e0
LFN <liangfengning@foxmail.com>
2021-06-20 14:13:02 +0800

    添加目录

--- src/templates/article.js
+++ src/templates/article.js
@@ -7,56 +7,28 @@ import Seo from "../components/seo"
 import "./article.less"
 import "./toc.less"
 
-const Meta = ({ frontmatter, html }) => {
+const Meta = ({ frontmatter }) => {
   const create = frontmatter.create_date
   const modify = frontmatter.date
   const titleId = frontmatter.title.replace(/[" "]/g, "-").toLowerCase()
 
   return (
     <section className="meta">
-      {/* <div className="meta-wrapper"> */}
-      {/* <div> */}
       <time title={`创建日期：${create} \n最后修改日期：${modify}`}>
         {create === modify ? "创建于" : "编辑于"} {modify}
       </time>
       <h1 id={titleId}>{frontmatter.title}</h1>
-      {/* <p>{frontmatter.description}</p> */}
-      {/*
-          <section className="actions">
-            <Link to="/">
-              <RiHome2Line aria-label="Back Icon" /> 返回主页
-            </Link>
-            {html.includes("</h2>") && (
-              <Link to="/">
-                <RiFileList2Line aria-label="Table of Contents Icon" /> 查看目录
-              </Link>
-            )}
-            <a
-              href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh"
-              target="_blank"
-              rel="noopener noreferrer"
-              title={`CC-BY-SA-4.0 署名-相同方式共享 4.0 国际`}
-              className="article-license pill"
-            >
-              <RiCopyrightLine aria-label="Copyright Icon" />
-              CC-BY-SA-4.0
-            </a>
-          </section> */}
-      {/* </div>
-        <div className="space"></div>
-      </div> */}
     </section>
   )
 }
 
-const TOC = ({ toc, html }) => {
+const TableOfContents = ({ toc }) => {
   return (
-    <section className="toc">
-      {html.includes("</h2>") && (
-        <div className="toc-wrapper">
-          <nav dangerouslySetInnerHTML={{ __html: toc }}></nav>
-        </div>
-      )}
+    <section className="toc" id="table-of-contents">
+      <details className="toc-wrapper">
+        <summary>文章目录</summary>
+        <nav dangerouslySetInnerHTML={{ __html: toc }}></nav>
+      </details>
     </section>
   )
 }
@@ -104,7 +76,12 @@ const ArticlePage = ({ data }) => {
         <Header back toc comment />
       </aside>
       <main>
-        <Meta frontmatter={post.frontmatter} html={post.html} />
+        <Meta frontmatter={post.frontmatter} />
+
+        {post.html.includes("</h2>") && (
+          <TableOfContents toc={post.tableOfContents} />
+        )}
+
         <article
           className="content"
           ref={articleRef}
