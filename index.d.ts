76cbdec12c409b12c20c868fd4908e1193244a42
梁峰宁 <liangfengning@foxmail.com>
2022-02-17 18:08:11 +0800

    修改主页样式

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,41 @@
 interface FrontMatter {
   name: string
   title: string
-  "create-date": string
+  'create-date': string
   date: string
   description: string
   license: string
-  serif?: string
 }
 
 interface Article extends FrontMatter {
   astro: {
     source: string
     html: string
-    headers: {
-      slug: number
-      depth: number
-      text: number
-    }[]
+    headers: Header[]
   }
   file: URL
   url: string
 }
 
+interface Header {
+  slug: number
+  depth: number
+  text: number
+}
+
 interface Group {
   [key: string]: Article[]
+}
+
+interface Friends {
+  data: { id: string; name: string; link: string; avatar: string }[]
+}
+
+interface Hero {
+  data: {
+    id: string
+    date_created: string
+    date_updated: string | null
+    markdown: string
+  }
 }
