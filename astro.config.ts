0325621020e5f12a5708e9e1f40a9b63a636cf7f
Lifeni <liangfengning@foxmail.com>
2023-07-03 00:17:06 +0800

    恢复 .npmrc

--- astro.config.ts
+++ astro.config.ts
@@ -7,8 +7,8 @@ import react from '@astrojs/react'
 import lit from '@astrojs/lit'
 
 export default defineConfig({
-  site: 'https://lifeni.life/',
   server: { port: 8000, host: true },
+  site: 'https://lifeni.life/',
   integrations: [
     unocss({ injectReset: true }),
     react(),
