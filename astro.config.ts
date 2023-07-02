f209e736a0930a88d1e53991d4a78bdcd1478bf8
Lifeni <liangfengning@foxmail.com>
2023-07-02 23:47:43 +0800

    移除占位文件

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 import unocss from 'unocss/astro'
+import vercel from '@astrojs/vercel/serverless'
+
+import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import lit from '@astrojs/lit'
 
 export default defineConfig({
-  adapter: vercel(),
+  adapter: vercel({}),
   output: 'server',
   site: 'https://lifeni.life/',
   integrations: [
