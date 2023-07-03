7981ec5310439739430f2a4ca3bc6d6438c3ff75
Lifeni <liangfengning@foxmail.com>
2023-07-03 23:18:07 +0800

    尝试修复 Vercel 部署

--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,12 @@ import vercel from '@astrojs/vercel/serverless'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
-const config = { injectReset: true }
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  server: { port: 8000, host: true },
   site: 'https://lifeni.life/',
-  adapter: vercel(),
-  integrations: [unocss(config), mdx(), sitemap()],
+  integrations: [unocss({ injectReset: true }), mdx(), sitemap()],
+  server: { port: 8000, host: true },
+  output: 'server',
+  adapter: vercel({ analytics: true, includeFiles: ['.vercel/output'] }),
+  build: { split: true },
 })
