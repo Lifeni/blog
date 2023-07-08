7404b2bbf51a95ab4783514a0b6104a2f6160d96
Lifeni <liangfengning@foxmail.com>
2023-07-08 13:18:44 +0800

    重命名 public 文件夹为 assets

--- astro.config.ts
+++ astro.config.ts
@@ -7,11 +7,12 @@ import unocss from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: { assets: true },
   site: 'https://lifeni.life/',
   integrations: [unocss({ injectReset: true }), mdx(), sitemap(), rome()],
   server: { port: 8000, host: true },
+  publicDir: 'assets',
   // https://github.com/withastro/astro/issues/7564
   output: 'server',
   adapter: vercel({ analytics: true }),
-  experimental: { assets: true },
 })
