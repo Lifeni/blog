a934d6b1131c3257b3a4f4e09430235037ad40f6
Lifeni <liangfengning@foxmail.com>
2023-07-09 21:12:57 +0800

    调整重定向参数

--- src/pages/echo.ts
+++ src/pages/echo.ts
@@ -1,8 +1,9 @@
 import type { APIRoute } from 'astro'
 
 export const get: APIRoute = ({ request, redirect }) => {
-  const host = request.headers.get('host')
-  const name = host?.split('.')[0] || ''
+  console.log(request.headers)
+  const referer = request.headers.get('referer')
+  const name = referer?.split('.')[0] || ''
   const domains = ['dev', 'lab', 'server']
 
   return redirect(domains.includes(name) ? `/${name}` : '/404', 302)
