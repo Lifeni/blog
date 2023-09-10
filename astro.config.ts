868f7e877547a77ec7a918a2c1647240dc9e9a17
Lifeni <liangfengning@foxmail.com>
2023-09-11 00:37:06 +0800

    再次重新设计界面

--- astro.config.ts
+++ astro.config.ts
@@ -1,46 +1,17 @@
-import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
-import unocss from 'unocss/astro'
-import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import rome from 'astro-rome'
-import compress from 'astro-compress'
-
-import external from 'rehype-external-links'
-import rewrite from 'rehype-rewrite'
+import svelte from '@astrojs/svelte'
+import partytown from '@astrojs/partytown'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({ analytics: true }),
-  experimental: { assets: true, viewTransitions: true },
-  site: 'https://lifeni.life',
-  redirects: { '/router': 'https://iokl.link' },
   server: { port: 8000, host: true },
-  integrations: [
-    unocss({ injectReset: true }),
-    react(),
-    mdx(),
-    sitemap(),
-    rome(),
-    compress(),
-  ],
-  markdown: {
-    shikiConfig: { theme: 'css-variables' },
-    rehypePlugins: [
-      [external, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
-      [
-        rewrite,
-        {
-          selector: 'h1',
-          rewrite: (n, _, p) => {
-            if (n.type !== 'element') return
-            p.children = p.children.filter(c => c.tagName !== 'h1')
-          },
-        },
-      ],
-    ],
-  },
+  site: 'https://lifeni.life',
+  output: 'server',
+  integrations: [svelte(), tailwind(), mdx(), sitemap(), partytown()],
+  adapter: vercel({ analytics: true }),
 })
