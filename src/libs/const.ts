f9006edbaaaa4d6482fb3dc1ffd9fccd310f8417
Lifeni <liangfengning@foxmail.com>
2023-10-21 23:45:42 +0800

    再次重新设计界面

--- src/libs/const.ts
+++ src/libs/const.ts
@@ -1,1 +1,9 @@
-export const fileBaseUrl = 'https://file.lifeni.life/notebook/'
+export const file = 'https://file.lifeni.life/notebook/'
+
+export const info = {
+  title: '记录干杯',
+  description:
+    '个人网站「记录干杯」，在这里记录一些技术相关的文章、尝试一些新的技术。',
+  color: '#feec44',
+  favicon: '/favicon.svg',
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+
+
 export interface Article {
   name: string
   subtitle?: string
