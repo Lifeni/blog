50df079e5703885c97737408456d8bb44e338975
LFN <liangfengning@foxmail.com>
2026-03-22 22:48:27 +0800

    增加 Vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
@@ -12,9 +13,12 @@ export default defineConfig({
   image: { domains: ['file.lifeni.life', 'octodex.github.com'] },
   integrations: [react(), mdx(), sitemap(), icon()],
   vite: { plugins: [tailwindcss()] },
+
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: { theme: 'css-variables' },
   },
+
   redirects,
+  adapter: vercel(),
 })
