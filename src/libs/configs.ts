6189a184522954a2329f0f63e3c53c05f0c8430a
LFN <liangfengning@foxmail.com>
2026-04-15 00:40:26 +0800

    优化专题卡片，限制主页展示文章数

--- src/libs/configs.ts
+++ src/libs/configs.ts
@@ -12,16 +12,16 @@ export const pages = [
     id: 'stories',
   },
   {
-    name: '存档',
-    href: '/archives',
-    icons: ['ri:folder-3-fill', 'ri:folder-open-fill'],
-    id: 'archives',
-  },
-  {
     name: '时刻',
     href: '/moments',
     icons: ['ri:camera-fill', 'ri:film-fill'],
     id: 'moments',
+  },
+  {
+    name: '存档',
+    href: '/archives',
+    icons: ['ri:folder-3-fill', 'ri:folder-open-fill'],
+    id: 'archives',
   },
 ]
 
